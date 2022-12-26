3d832019a41590715a0f64450776f503261f18dd
Andrey Risukhin Vostro WSL <andrey.risukhin@outlook.com>
2022-12-25 21:56:17 -0800

    Continue with "get all legal moves"

--- main.py
+++ main.py
@@ -29,10 +29,15 @@ cards = {}
 # TODO ^
 
 class Domino():
+    """
+    Stores two faces, each a tuple of (area_code:str, crown_count:int).
+    """
     def __init__(self, id:int, area_1:str, crowns_1:int, area_2:str, crowns_2:int):
         self.id = id
-        self.area_1, self.crowns_1 = area_1, crowns_1
-        self.area_2, self.crowns_2 = area_2, crowns_2
+        # self.area_1, self.crowns_1 = area_1, crowns_1
+        # self.area_2, self.crowns_2 = area_2, crowns_2
+        self.face_1 = (area_1, crowns_1)
+        self.face_2 = (area_2, crowns_2)
 
     def __repr__(self):
         return f"Card {self.id}: {self.area_1} {self.crowns_1} {self.area_2} {self.crowns_2}"
@@ -41,7 +46,11 @@ class Domino():
         return self.id
 
     def get_face_1(self):
-        return f'{self.area_1}'
+        return self.face_1
+    
+    def get_face_2(self):
+        return self.face_2
+
 
 class Board():
     """
@@ -52,7 +61,7 @@ class Board():
     Has methods to interact, with logic to allow legal moves.
     """
     def __init__(self):
-        self.grid = [['x' for i in range(5)] for i in range(5)]
+        self.grid = [[('x', 0) for i in range(5)] for i in range(5)]
         self.has_castle = False
 
     def get_grid(self):
@@ -63,17 +72,50 @@ class Board():
         Place the castle at coordinates (row, col).
         Update internal castle variable, precondition to playable game.
         """
-        self.grid[row][col] = 'castle'
+        assert 0 <= row and row < 5 and 0 <= col and col < 5, "Out of Board Castle Request"
+
+        self.grid[row][col] = ('castle', 0)
         self.has_castle = True
+
+
+    def get_legal_coords(self, d:Domino):
+        """
+        TODO
+        Return list of coordinates to place this domino on the board legally.
+            If no locations valid, return the option to skip this piece.
+        """
+        return []
+
+        # CONTINUE HERE
+
+
+
 
     def put_domino(self, d:Domino, r1:int, c1:int, r2:int, c2:int):
         """
         Place domino d on grid.
-            Area 1 specified by (row 1, col 1), area 2 by (r2,c2).
+            Face 1 specified by (row 1, col 1), face 2 by (r2,c2).
         """
-        pass 
+        assert 0 <= r1 and r1 < 5 and 0 <= c1 and c1 < 5, "Out of Board Face 1 coords"
+        assert 0 <= r2 and r2 < 5 and 0 <= c2 and c2 < 5, "Out of Board Face 2 coords"
 
+        is_legal = False
+        # Check legality
+        a1 = d.get_face_1()[0]
+        a2 = d.get_face_2()[0]
 
+        adj_a1 = []
+        if 0 <= r1 - 1 and 
+
+        # TODO There has to be a cleaner way here, the connected island leetcode problem
+        # Wonder if could reuse this "identify an island" code to score? possibly not, different decisions (contiguous chunk vs is adjacent equal type)
+
+    # def is_legal_domino_put():
+
+    # TODO discard a domino if cannot put anywhere
+
+    # TODO a "where can I put this domino?" that returns list of coords that are valid , would this be useful?
+    # Strongly think this needs to be implemented, for game logic of discard piece option
 
 def create_card_map(filepath):
     """
