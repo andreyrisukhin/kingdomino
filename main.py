687cac13f1faa39d63c5a80c8ae93c0442fbdf71
Andrey Risukhin Vostro WSL <andrey.risukhin@outlook.com>
2023-01-02 16:30:29 -0800

    Time for Monte carlo trees!

--- main.py
+++ main.py
@@ -13,64 +13,6 @@ Recall Thompson Sampling worked better in CSE 312 on HW.
 
 from kingdomino import *
 
-# TODO DELETE THESE, DUPLICATED IN GAME MANAGER
-def create_card_map(filepath):
-    """
-    Takes the filepath to create the cards from.
-    Returns a map of card IDs (as in game) to card faces.
-    """
-    cards = {}
-    with open(file=filepath, mode='r') as f:
-        contents = f.readlines()
-        header = contents[0]
-        card_list = contents[1:]
-        print(f'header: {header}')
-        print(f'card_list: {card_list}')
-        # Process the file
-        for line in card_list:
-            id, card = line.split(" ", maxsplit=1)
-            card, _ = card.split("\n")
-            a1, c1, a2, c2 = card.split(" ", maxsplit=3)
-            cards[int(id)] = Domino(id, area_1=a1, crowns_1=int(c1), area_2=a2, crowns_2=int(c2))
-    return cards
-
-def shuffle(num_cards):
-    """
-    Takes an int representing the total number of cards.
-    Returns a random sequence of card IDs.
-    """
-    deck = np.arange(start=1, stop=num_cards)
-    shuffled = rng.permutation(deck)
-    return shuffled
-
-# === Local Testing Below ====
-
-# cards = create_card_map("./cards.txt")
-# shuffled = shuffle(len(cards))
-# print(cards[shuffled[0]])
-
-# print(shuffled[:4])
-# print(sorted(shuffled[:4]))
-
-# b = Board()
-# print(b)
-# # b.put_castle(2,2)
-# b.put_castle(4,4)
-# print(b)
-
-# for i in range(12):
-#     di = cards[shuffled[i]]
-#     di_places = b.get_legal_coords(di)
-
-#     if not di_places == []: # Otherwise if no legal moves, skip placing this card
-#         b.put_domino(di, di_places[0])
-
-#     print(f'di: {di}')
-#     print(f'{len(di_places)} moves')
-#     print(b)
-
-# print(b.get_score())
-
 
 gm = GameManager(cardpath="./cards.txt")
 
@@ -80,5 +22,14 @@ for i in range(n):
     gi_scores = gm.new_game()
     game_outcomes.append(gi_scores)
 
-print(f'Outcomes from {n} games')
-print(game_outcomes)
+# print(f'Outcomes from {n} games')
+# print(game_outcomes)
+
+"""
+Analysis of scores
+"""
+# for g in game_outcomes:
+#     total_score = sum(g)
+#     print(total_score)
+
+
